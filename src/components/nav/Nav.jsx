ab63ff02ed02c0991a7209b3dbbd3894bd632a0c
otabek <otajongold@gmail.com>
2024-06-22 05:24:15 +0500

    first commit

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Nav.css"
+
+const Nav = () => {
+  return (
+    <div className='container'>
+         <div className="nav">
+               <h1>Hello</h1>
+         </div>
+    </div>
+  )
+}
+
+export default Nav
